97ac112ec79195c4ee9af174581444ee01fec78c
just-Hms <alessandroversari1@gmail.com>
2022-10-20 01:13:01 +0200

    fix primary menu query selector error

--- resources/js/app.js
+++ resources/js/app.js
@@ -39,9 +39,10 @@ function toggles() {
 
 	// nav toggle
 
-	let main_navigation = document.querySelector('#primary-menu');
+	const main_navigation = document.querySelector('#primary-menu');
+	const primary_menu_toggle = document.querySelector('#primary-menu-toggle');
 	
-	main_navigation.addEventListener('click', function (e) {
+	primary_menu_toggle.addEventListener('click', function (e) {
 		e.preventDefault();
 		main_navigation.classList.toggle('hidden');
 	});
